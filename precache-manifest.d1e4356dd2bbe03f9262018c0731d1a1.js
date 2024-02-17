8899bc1777746e5b879d8f50cf0d017216f01dc3
Hamid Bashiri <hvbashiri@gmail.com>
2024-02-17 22:19:40 +0330

    Updates

---
+++ precache-manifest.d1e4356dd2bbe03f9262018c0731d1a1.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6cf8efe487e76a9931aee0e218726b6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8d6ce89ab6fd2bb5e4ac",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "ba18219f0c7bcfe8d6c5",
+    "url": "/portfolio/static/css/main.334461f7.chunk.css"
+  },
+  {
+    "revision": "8d6ce89ab6fd2bb5e4ac",
+    "url": "/portfolio/static/js/2.c5d3806a.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.c5d3806a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba18219f0c7bcfe8d6c5",
+    "url": "/portfolio/static/js/main.d8b02e54.chunk.js"
+  },
+  {
+    "revision": "6c972d9418aa5b4e59ab",
+    "url": "/portfolio/static/js/runtime-main.1b893ad1.js"
+  },
+  {
+    "revision": "dcdeeadcd1082b2fac0c404e9cfdd1af",
+    "url": "/portfolio/static/media/1st.dcdeeadc.png"
+  },
+  {
+    "revision": "1167fffc243b49c88cbb3545ba5493b4",
+    "url": "/portfolio/static/media/2st.1167fffc.png"
+  },
+  {
+    "revision": "f7f8a68e41b68eaf07e36cd6597dd9fc",
+    "url": "/portfolio/static/media/DominoColorBanner.f7f8a68e.png"
+  },
+  {
+    "revision": "be0dde96594ef4390f2b120d80318d5f",
+    "url": "/portfolio/static/media/HexaDaminoesIcon.be0dde96.png"
+  },
+  {
+    "revision": "1c0f93281e72d3860f0562fc7a809fe4",
+    "url": "/portfolio/static/media/Icon.1c0f9328.jpg"
+  },
+  {
+    "revision": "d7e4a8f488ccde22a01e0a01322ff93f",
+    "url": "/portfolio/static/media/Rebel1.d7e4a8f4.png"
+  },
+  {
+    "revision": "5dc37ea3fb3bcf9cd64073cc218f0743",
+    "url": "/portfolio/static/media/Rebel2.5dc37ea3.png"
+  },
+  {
+    "revision": "7a887b14fed9035c56fd54f3d7e1d2c2",
+    "url": "/portfolio/static/media/Rebel3.7a887b14.png"
+  },
+  {
+    "revision": "1e1b7ef55fd449412c957bf9eab49b81",
+    "url": "/portfolio/static/media/Rebel4.1e1b7ef5.png"
+  },
+  {
+    "revision": "31f9ace4b6a277d6a1c0b9ce66f08a22",
+    "url": "/portfolio/static/media/Rose.31f9ace4.jpg"
+  },
+  {
+    "revision": "1e5672f695b279ce5135c59ef7e6421c",
+    "url": "/portfolio/static/media/UT_logo.1e5672f6.png"
+  },
+  {
+    "revision": "83956ab0a68aada3503f068d70986a99",
+    "url": "/portfolio/static/media/V1.83956ab0.jpg"
+  },
+  {
+    "revision": "182a4dca59d80a71db0e995bd3a6a3c9",
+    "url": "/portfolio/static/media/Vahid.182a4dca.webp"
+  },
+  {
+    "revision": "9dbd475a8f095d3d567ffbf0f95b5e14",
+    "url": "/portfolio/static/media/f_cload1.9dbd475a.png"
+  },
+  {
+    "revision": "dcdeeadcd1082b2fac0c404e9cfdd1af",
+    "url": "/portfolio/static/media/gameplay1.dcdeeadc.png"
+  },
+  {
+    "revision": "1167fffc243b49c88cbb3545ba5493b4",
+    "url": "/portfolio/static/media/gameplay2.1167fffc.png"
+  },
+  {
+    "revision": "71eed30bc10976f8455eba3fff8668ff",
+    "url": "/portfolio/static/media/gameplay3.71eed30b.png"
+  },
+  {
+    "revision": "2a5b656719b0e7af17f781a941e8e71f",
+    "url": "/portfolio/static/media/iust.2a5b6567.png"
+  },
+  {
+    "revision": "eec3bc083838afcf5c35d62f38c7bf49",
+    "url": "/portfolio/static/media/lake.eec3bc08.png"
+  },
+  {
+    "revision": "18d0b91f40bc64e7ffbaa1044316d272",
+    "url": "/portfolio/static/media/leangoLogo.18d0b91f.png"
+  },
+  {
+    "revision": "b857847293a228f1f889f334fb6f72a6",
+    "url": "/portfolio/static/media/mushroom.b8578472.png"
+  },
+  {
+    "revision": "225f03e52b38d770edc5072041bc23cf",
+    "url": "/portfolio/static/media/playground_icon.225f03e5.jpg"
+  },
+  {
+    "revision": "5d2f3f40abb65fbb948a64783a18c6fd",
+    "url": "/portfolio/static/media/rebel_forground.5d2f3f40.gif"
+  },
+  {
+    "revision": "07119f7daa1dcca728ec26c89402b0e0",
+    "url": "/portfolio/static/media/sky.07119f7d.gif"
+  },
+  {
+    "revision": "a9070dd5ec1fb7357f5add96a53e3aa3",
+    "url": "/portfolio/static/media/spidercube.a9070dd5.png"
+  },
+  {
+    "revision": "994e5caf01d2d46a2e8ac1fb455e52c1",
+    "url": "/portfolio/static/media/waterfall0.994e5caf.jpg"
+  },
+  {
+    "revision": "dc02405d143d3f24d09c39f05ac9f197",
+    "url": "/portfolio/static/media/waterfall1.dc02405d.png"
+  },
+  {
+    "revision": "f4531fc74bd0754e4b33cf7f6a8bed45",
+    "url": "/portfolio/static/media/waterfall2.f4531fc7.png"
+  }
+]);
